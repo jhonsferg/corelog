3309bbca0f10cbdf696e9cadc338842627ee86ac
jhonsferg <jhon.fernandez.sg@gmail.com>
2026-09-17 16:29:54 -0500

    Vite configuration for path resolution using aliases

--- ui/vite.config.ts
+++ ui/vite.config.ts
@@ -1,11 +1,17 @@
+import { fileURLToPath, URL } from 'node:url'
+
 import react, { reactCompilerPreset } from '@vitejs/plugin-react'
 import babel from '@rolldown/plugin-babel'
 import { defineConfig } from 'vite'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [
     react(),
     babel({ presets: [reactCompilerPreset()] })
   ],
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
+    },
+  },
 })
